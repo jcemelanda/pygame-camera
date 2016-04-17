9e9f3614d953b359ea057fc3f7bc2301186a1f81
jcemelanda <jcemelanda@localhost.localdomain>
2016-04-17 13:44:26 -0300

    Readme and new package name

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='pygame_camera',
     packages=['pygame_camera'],
-    version='0.1',
+    version='0.1.1',
     description='A Camera lib for PyGame',
     author='Julio Melanda',
     author_email='jcemelanda@gmail.com',
     url='https://github.com/jcemelanda/pygame-camera',
-    download_url='https://github.com/jcemelanda/pygame-camera/tarball/0.1',
+    download_url='https://github.com/jcemelanda/pygame-camera/tarball/0.1.1',
     keywords=['pygame', 'camera', 'gamedev'],
     classifiers=[],
 )
